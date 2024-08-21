36db9ce06a8a8b1cfd2698d4fd18c214d68aed44
VastSky0 <chuongsohc@gmail.com>
2024-08-22 03:43:26 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -6,7 +6,7 @@ echo -e "\e[1;33mĐang cài đặt\e[0m"
 apt update && apt upgrade -y
 clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/VastSky0/4g0d/raw/main/4g0d.zip && unzip 4g0d.zip && chmod a+x nek && chmod a+x 4g && chmod a+x psiphon-tunnel-core && chmod a+x qex && chmod a+x auto && mv auto login && clear
-sleep 2
+sleep 1
 echo -e "\e[1;32mĐã cài đặt thành công\e[0m"
 sleep 1
 clear
