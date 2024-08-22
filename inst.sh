b07b5fc2c9ff79735ecf48d50b85f8ea1acc0f26
VastSky0 <chuongsohc@gmail.com>
2024-08-23 02:32:13 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -1,11 +1,6 @@
 echo ""
-echo -e "\e[1;33mCấp quyền truy cập bộ nhớ thiết bị\e[0m"
-echo ""
-sleep 1
-termux-setup-storage
-echo ""
 echo -e "\e[1;33mCập nhật packages\e[0m"
 echo ""
 pkg update -y
 echo ""
-echo -e "\e[1;33mĐang cập nhật pakages\e[0m"
+sleep 1
