d0678f0097928c6dc6c1c915c6c2674b92d0907b
VastSky0 <chuongsohc@gmail.com>
2024-08-23 21:49:46 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -9,11 +9,11 @@ apt update && apt upgrade -y
 clear
 echo -e "\e[1;33mĐang cài đặt\e[0m"
 sleep 1
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gVT/raw/main/4g0d.zip && unzip 4g0d.zip && chmod a+x *  
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/VastSky0/4g0d/raw/main/4g0d.zip && unzip 4g0d.zip && chmod a+x *  
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/acheng888/4gVT/raw/main/t1 && mv t1 motd
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/VastSky0/4g0d/raw/main/t1 && mv t1 motd
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gVT/raw/main/l1 && chmod +x * && mv l1 login &&
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/VastSky0/4g0d/raw/main/l1 && chmod +x * && mv l1 login &&
 clear
 sleep 1
 echo -e "\e[1;32mĐã cài đặt thành công\e[0m"
