32c33ba207f2da9287874982bd207066ec51d2de
VastSky0 <chuongsohc@gmail.com>
2024-08-25 00:54:36 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -1,13 +1,13 @@
 clear
-echo -e "\e[1;33mCấp quyền truy cập bộ nhớ thiết bị\e[0m"
+echo -e "\e[1;93mCấp quyền truy cập bộ nhớ thiết bị\e[0m"
 sleep 1
 termux-setup-storage
 clear
 sleep 2
-echo -e "\e[1;33mCập nhật packages\e[0m"
+echo -e "\e[1;93mCập nhật packages\e[0m"
 apt update && apt upgrade -y
 clear
-echo -e "\e[1;33mĐang cài đặt\e[0m"
+echo -e "\e[1;93mĐang cài đặt\e[0m"
 sleep 1
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/VastSky0/test/raw/main/4g0d.zip && unzip 4g0d.zip && chmod a+x *
 clear
@@ -16,7 +16,7 @@ clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/VastSky0/test/raw/main/rtb && chmod +x * && mv rtb login
 clear
 sleep 1
-echo -e "\e[1;32mĐã cài đặt thành công\e[0m"
+echo -e "\e[1;92mĐã cài đặt thành công\e[0m"
 sleep 1
 clear
 termux-wake-lock
